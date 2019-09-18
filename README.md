4dfb4314c68a1e9e8482bca34b4b09b678a86e46
yehudah <ykoblick@tribextech.com>
2019-09-17 22:44:27 -0400

    second commit

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
-Sample Chat App to illustrate the fundementals of
+Sample Chat App to illustrate the fundamentals of
 GraphQL subscriptions
 
-TODO
+## TODO:
+
+Add mongoose and db persistance and see how the typedef map to it
+
+
+##
+
+This project references the following tutorial
+
+https://pusher.com/tutorials/chat-graphql-subscriptions
--- server/src/schema.js
+++ server/src/schema.js
@@ -1,8 +1,12 @@
 // server/src/schema.js
 
+/*
+
+We start by defining a simple Chat type, which has three fields: the chat ID, the username of the user sending the message and the message itself. Then we define a query to fetch all messages and a mutation for sending a new message, which accepts the username and the message. Lastly, we define a subscription, which we are calling messageSent and it will return a message.
+
+*/
 const typeDefs = `
   
-    //Should sync up with 
   type Chat {
     id: Int!
     from: String!
